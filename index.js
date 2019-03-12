6e52ef88ab9ae2f2ab5dac33d23658b5454b5365
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-03-11 18:14:59 -0700

    index.js modified

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const month = date.toLocaleDateString('en-us',{month:'long'});
 const h1 =document.createElement('h1');
 h1.style.color = 'lime';
 h1.innerHTML.text = month;
-h1.style[`text-shadow`] = 3px 2px 3px black;
+h1.style[`text-shadow`] = `3px 2px 3px black`;
 body.appendChild(h1);
 
 //how many weeks 
@@ -21,12 +21,18 @@ body.style.padding= '1em';
 const container = document.createElement('div');
 body.appendChild(container);
 container.style.width = '720px';
-container.style.height = (week * 100) + 'px';
+container.style.height = (weeks * 100) + 'px';
 container.style.display = 'flex';
 body.style.flexDirection = 'column';
 body.style.alignItems = 'center';
 const today = new Date().getMonth();
 const firstDay = new Date (`${today + 1}/01/${new Date().getFullYear()}`).getDay();
-= new Date(`${today + 1 + 1}/01/${new Date().getFullYear()}`);
+const nextMonth = new Date(`${today + 1 + 1}/01/${new Date().getFullYear()}`);
 nextMonth.setDate(nextMonth.getDate() -1);
 const lastDay = nextMonth.getDate();
+let dayNumber = 0;
+console.log(firstDay,'first day');
+console.log('last day',lastDay);
+for (let i =0; i < days; i++) {
+    
+}
